fe827390164cf171f6c9acb2886222a7641bbf7f
Jan Brits <pcerservices@gmail.com>
2019-01-13 07:38:24 +0000

    working ecommerce site

---
+++ products/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from .models import Product
+
+class ProductTests(TestCase):
+    """
+    Here we'll define the tests that we'll run against our product models
+    """
+    
+    def test_str(self):
+        test_name = Product(name='A Product')
+        self.assertEqual(str(test_name), 'A Product')
